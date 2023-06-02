b79404dc53652dfb6294954ebbf5ce3a04f8f59c
r3zar <rrahimi25@gmail.com>
2023-06-02 16:07:04 +0330

    QuickSort

--- src/main/java/org/algorithm/quicksort/MainClass.java
+++ src/main/java/org/algorithm/quicksort/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
     public static void main(String[] args) {
 
         Integer[] arr = {50,7,2,20,4,14,13};
-        System.out.println(SumArray.recursiveSum(arr , arr.length -1));
+//        System.out.println(SumArray.recursiveSum(arr , arr.length -1));
         new QuickSort().sort(arr);
         System.out.println(Arrays.toString(arr));
 
